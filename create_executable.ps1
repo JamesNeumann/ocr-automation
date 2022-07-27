e6f4341a6d25cdc65ce8d4e5c34a7a8b3f115335
James Neumann <41788164+JamesNeumann@users.noreply.github.com>
2022-07-27 23:14:25 +0200

    Remove spaces

--- create_executable.ps1
+++ create_executable.ps1
@@ -1,7 +1,7 @@
 ./venv/Scripts/activate
 Write-Host -ForegroundColor Blue "Activating virtual environment"
 Write-Host -ForegroundColor Blue "Building executable"
-pyinstaller OcrAutomation.spec --version-file = version.txt -y
+pyinstaller OcrAutomation.spec --version-file=version.txt -y
 Write-Host -ForegroundColor Blue "Deactivating virtual environment"
 Write-Host -ForegroundColor DarkGreen "Done!"
 deactivate
